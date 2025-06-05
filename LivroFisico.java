2a330aa61c93f182534a46989fbae2984f9a2757
Ana Luiza Carlesso <alcarlesso3@gmail.com>
2025-06-05 19:02:19 -0300

    LivroFisico.java

---
+++ LivroFisico.java
@@ -0,0 +1,23 @@
+public class LivroFisico extends Livro {
+    private int numeroDePaginas;
+
+    public LivroFisico(String titulo, int anoPublicacao, Autor autor, int numeroDePaginas) {
+        super(titulo, anoPublicacao, autor);
+        this.numeroDePaginas = numeroDePaginas;
+    }
+
+    public int getNumeroDePaginas() {
+        return numeroDePaginas;
+    }
+
+    public void setNumeroDePaginas(int numeroDePaginas) {
+        this.numeroDePaginas = numeroDePaginas;
+    }
+
+    @Override
+    public void exibirDetalhes() {
+        System.out.println("Livro Físico - Título: " + getTitulo() + ", Ano: " + getAnoPublicacao() +
+                           ", Páginas: " + numeroDePaginas);
+        System.out.println("Autor: " + getAutor().getNome() + ", Nacionalidade: " + getAutor().getNacionalidade());
+    }
+}
